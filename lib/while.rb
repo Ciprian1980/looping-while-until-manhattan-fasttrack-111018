6f8471b991c746e0c462444af4bccf1a8aef8914
Ciprian Popa <ciprian_popa2002@yahoo.com>
2018-11-11 15:00:52 +0000

    automatically backed up by learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,11 +1,11 @@
 def using_while
+  levitation_force = 6
   
   while 
-    levitation_force = 6
-	
-  until counter < 10 
+    levitation_force < 10 
     puts "We are getting better with ruby.Yahooo!!!"
-    counter+=1
+    levitation_force += 1
+    
 end
 
 
